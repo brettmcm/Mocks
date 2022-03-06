e29ede245211b981b2b4bf7d3f9e3fe75dd41c68
Brett McMillin <brett@radsmth.com>
2022-03-06 12:39:40 -0800

    Initial Commit

---
+++ Mocks/MocksApp.swift
@@ -0,0 +1,17 @@
+//
+//  MocksApp.swift
+//  Mocks
+//
+//  Created by Brett McMillin on 3/6/22.
+//
+
+import SwiftUI
+
+@main
+struct MocksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
